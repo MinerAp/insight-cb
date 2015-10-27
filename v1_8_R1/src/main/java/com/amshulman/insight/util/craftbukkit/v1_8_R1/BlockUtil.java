dd6ca6553b89b31360a22608e6ea4832c8141f21
Andrew Shulman <andy.shulman@hotmail.com>
2015-10-26 23:40:48 -0400

    Fix willIgnite check on 1.8.x

--- v1_8_R1/src/main/java/com/amshulman/insight/util/craftbukkit/v1_8_R1/BlockUtil.java
+++ v1_8_R1/src/main/java/com/amshulman/insight/util/craftbukkit/v1_8_R1/BlockUtil.java
@@ -2,13 +2,16 @@ package com.amshulman.insight.util.craftbukkit.v1_8_R1;
 
 import javax.annotation.Nonnull;
 
+import net.minecraft.server.v1_8_R1.BlockPosition;
+import net.minecraft.server.v1_8_R1.Blocks;
+
 import org.bukkit.block.Block;
-import org.bukkit.craftbukkit.v1_8_R1.util.CraftMagicNumbers;
+import org.bukkit.craftbukkit.v1_8_R1.CraftWorld;
 
 public final class BlockUtil extends com.amshulman.insight.util.craftbukkit.BlockUtil {
 
     @Override
     public boolean willIgnite(@Nonnull Block block) {
-        return CraftMagicNumbers.getBlock(block).d();
+        return Blocks.FIRE.e(((CraftWorld) block.getWorld()).getHandle(), new BlockPosition(block.getLocation().getX(), block.getLocation().getY(), block.getLocation().getZ()));
     }
 }
--- v1_8_R3/src/main/java/com/amshulman/insight/util/craftbukkit/v1_8_R3/BlockUtil.java
+++ v1_8_R3/src/main/java/com/amshulman/insight/util/craftbukkit/v1_8_R3/BlockUtil.java
@@ -2,13 +2,16 @@ package com.amshulman.insight.util.craftbukkit.v1_8_R3;
 
 import javax.annotation.Nonnull;
 
+import net.minecraft.server.v1_8_R3.BlockPosition;
+import net.minecraft.server.v1_8_R3.Blocks;
+
 import org.bukkit.block.Block;
-import org.bukkit.craftbukkit.v1_8_R3.util.CraftMagicNumbers;
+import org.bukkit.craftbukkit.v1_8_R3.CraftWorld;
 
 public final class BlockUtil extends com.amshulman.insight.util.craftbukkit.BlockUtil {
 
     @Override
     public boolean willIgnite(@Nonnull Block block) {
-        return CraftMagicNumbers.getBlock(block).d();
+        return Blocks.FIRE.e(((CraftWorld) block.getWorld()).getHandle(), new BlockPosition(block.getLocation().getX(), block.getLocation().getY(), block.getLocation().getZ()));
     }
 }
